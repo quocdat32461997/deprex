dbe00500803eef3c820c269652221876cf1634af
Eric Ngo <quocdat32461997@yahoo.com>
2020-09-10 20:59:11 -0500

    Initiali commit: add main.py and gitignore

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+main.py - module to implement Python Reddit API Wrapper to crawl text data of depression and relevant topics
+"""
+
+# import dependencies
+import argparse
+import os
+
+def main(args):
+	
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description = 'Argument Parser')
+	main(args.parse_args())
